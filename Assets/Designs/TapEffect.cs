f2715a60eaa27907c59f67b0e32f4a508a672dad
itoka229 <itoka229mori@gmail.com>
2022-10-22 03:54:23 +0900

    タイトルのUI周りをヒズモが作ったものに変更

--- Assets/Designs/TapEffect.cs
+++ Assets/Designs/TapEffect.cs
@@ -9,23 +9,23 @@ public class TapEffect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       particleSystem=GetComponent<ParticleSystem>();
-       camera=Camera.main; 
+        particleSystem = GetComponent<ParticleSystem>();
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount>0){
-            Touch touch=Input.GetTouch(0);
-         if(touch.phase==TouchPhase.Began){
-            Vector3 touchposition=touch.position;
-            touchposition.z=10;
-            transform.position=camera.ScreenToWorldPoint(touchposition);
-            particleSystem.Play();
-         }   
-
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 touchposition = touch.position;
+                touchposition.z = 10;
+                transform.position = camera.ScreenToWorldPoint(touchposition);
+                particleSystem.Play();
+            }
         }
-
     }
 }
