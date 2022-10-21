d2e881db199454614e29178772dbcbab235f561c
hizumo <104607366+hizumo@users.noreply.github.com>
2022-10-22 03:36:20 +0900

    タッチした時のパーティクル

---
+++ Assets/Designs/TapEffect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapEffect : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+    private Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+       particleSystem=GetComponent<ParticleSystem>();
+       camera=Camera.main; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0){
+            Touch touch=Input.GetTouch(0);
+         if(touch.phase==TouchPhase.Began){
+            Vector3 touchposition=touch.position;
+            touchposition.z=10;
+            transform.position=camera.ScreenToWorldPoint(touchposition);
+            particleSystem.Play();
+         }   
+
+        }
+
+    }
+}
