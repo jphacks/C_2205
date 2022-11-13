dfbf812a28f84dcac5a6407cf65c049344db40ae
hizumo <104607366+hizumo@users.noreply.github.com>
2022-11-13 15:03:28 +0900

    パーティクル変更

---
+++ Assets/Scripts/AuraGenerator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuraGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void GenerateAura(Vector3 auraPosition)
+    {
+
+    }
+
+}
