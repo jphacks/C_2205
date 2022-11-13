191a436750fca9df3a01300bd5e368b9b64716c1
hizumo <104607366+hizumo@users.noreply.github.com>
2022-11-13 16:12:04 +0900

    位置共有完成

--- Assets/Scripts/AuraGenerator.cs
+++ Assets/Scripts/AuraGenerator.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class AuraGenerator : MonoBehaviour
 {
+    private float elapsedTime;
+    [SerializeField] private float interval;
+    private AllPlayerPositionInfoForKehai allPlayerPositionInfoForKehai;
+    [SerializeField] GameObject particlePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        allPlayerPositionInfoForKehai = GetComponent<AllPlayerPositionInfoForKehai>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsedTime += Time.deltaTime; 
+        if(elapsedTime > interval)
+        {
+            GenerateAura();
+            elapsedTime = 0;
+        }
     }
-    private void GenerateAura(Vector3 auraPosition)
+    private void GenerateAura()
     {
-
+        foreach (Vector3 auraPosition in allPlayerPositionInfoForKehai.GetAllPlayerPosition())
+        {
+            Instantiate(particlePrefab, auraPosition, Quaternion.identity);
+        }
     }
 
 }
